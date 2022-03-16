4f4d9056f308217aca30163e6033b2029e684d35
Thanh <tnkchaseme@gmail.com>
2022-03-16 18:52:56 -0500

    third

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from flask_pymongo import PyMongo
 from flask import redirect, url_for, Flask, render_template, request
 
 app = Flask(__name__)
-# app.config['MONGO_URI'] = os.environ.get("MONGODB_URI")
-app.config[
-    'MONGO_URI'] = 'mongodb+srv://tnkchaseme:Collin611194@cluster0.q5bm7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = os.environ.get("MONGODB_URI")
 mongo = PyMongo(app)
 db = mongo.db
 Bootstrap(app)
